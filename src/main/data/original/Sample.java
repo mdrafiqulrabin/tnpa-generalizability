557776c6707be71318b75730cd0d8aac74db33bc
mdrafiqulrabin <mdrafiqulrabin@gmail.com>
2019-06-27 19:15:18 -0500

    Sample input from code2vec online demo

--- src/main/data/original/Sample.java
+++ src/main/data/original/Sample.java
@@ -1,14 +1,14 @@
 class C {
-    void f(int[] var0) {
-        boolean var1 = true;
-        for (int var2 = 0; var2 < var0.length && var1; var2++) {
-            var1 = false;
-            for (int var3 = 0; var3 < var0.length - 1 - var2; var3++) {
-                if (var0[var3] > var0[var3 + 1]) {
-                    int var4 = var0[var3];
-                    var0[var3] = var0[var3 + 1];
-                    var0[var3 + 1] = var4;
-                    var1 = true;
+    void f(int[] array) {
+        boolean swapped = true;
+        for (int i = 0; i < array.length && swapped; i++) {
+            swapped = false;
+            for (int j = 0; j < array.length - 1 - i; j++) {
+                if (array[j] > array[j+1]) {
+                    int temp = array[j];
+                    array[j] = array[j+1];
+                    array[j+1]= temp;
+                    swapped = true;
                 }
             }
         }
